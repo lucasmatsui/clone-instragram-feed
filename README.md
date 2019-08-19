a04df550c2940d3eb392dc27056b453dc6b1d6ee
Lucas Kenzo <lucas-matsui@hotmail.com>
2019-08-19 17:51:43 -0300

    clone instagram

---
+++ README.md
@@ -0,0 +1,9 @@
+#Clone do instagram
+
+- Interface criada com React Native :D
+
+##Iniciar
+
+- Para iniciar apenas execute: npm install.
+- Para simular uma api usamos a dependencia json-server, execute:
+-- yarn json-server server.json -d 1000 -w
---
+++ src/components/LazyImage/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Animated } from 'react-native';
+
+import { Small, Original } from './styles';
+
+const OrignalAnimated = Animated.createAnimatedComponent(Original);
+
+export default function LazyImage({
+  smallSource,
+  source,
+  aspectRatio,
+  shouldLoad,
+}) {
+  const opacity = new Animated.Value(0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(()  => {
+    if (setLoaded) {
+      setLoaded(true);
+    }
+  }, [shouldLoad]);
+
+  function handleAnimate() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  return (
+    <Small
+      source={smallSource} 
+      ratio={aspectRatio} 
+      resizeMode="contain"
+      blurRadius={1}
+    >
+      {loaded && <OrignalAnimated 
+        style={{ opacity }}
+        source={source} 
+        ratio={aspectRatio} 
+        resizeMode="contain" 
+        onLoadEnd={handleAnimate}
+      />}
+
+    </Small>
+  );
+}
