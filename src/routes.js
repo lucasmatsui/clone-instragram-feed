cdeba6d2722d8cf56ca3c5fe72e83292086f8d50
Lucas Kenzo <lucas-matsui@hotmail.com>
2019-08-21 00:51:45 -0300

    Adicionado os icones do header

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Image } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import logo from './assets/instagram.png';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Feed from './pages/Feed';
 
@@ -13,9 +14,18 @@ const Routes = createAppContainer(
     headerLayoutPreset: 'center',
     defaultNavigationOptions:{
       headerTitle: <Image source={logo} />,
+      headerLeft: <Icon name="camera-alt" size={28} color="#444" onPress={() => {alert("Abrir Camera!");}}/>,
+      headerRight: <Icon name="launch" size={28} color="#444" onPress={() => {alert("Ir para o Direct!");}}  />,
       headerStyle: {
-        backgroundColor: '#f5f5f5'
+        backgroundColor: '#f3f3f3',
+        height:45
       },
+      headerLeftContainerStyle: {
+        paddingLeft: 15
+      },
+      headerRightContainerStyle: {
+        paddingRight: 15
+      }
     },
   })
 );
